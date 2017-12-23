e6870f58aff7e15984b5539856e7c4fd469afefd
Erny Sans <ernysans@icloud.com>
2017-12-23 03:34:56 -0500

    Update config

--- firestore-persistence.js
+++ firestore-persistence.js
@@ -1,15 +1,16 @@
 (function() {
   "use strict";
-
-  if (typeof firebase === 'undefined') {
-    throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before firebase-init.html');
-  }
-  firebase.firestore().enablePersistence()
-    .catch(function(err) {
-      if (err.code === 'failed-precondition') {
-        throw new Error('Multiple tabs open, persistence can only be enabled in one tab at a a time.');
-      } else if (err.code === 'unimplemented') {
-        throw new Error('The current browser does not support all of the features required to enable persistence');
-      }
-    });
+  setTimeout(() => {
+    if (typeof firebase === 'undefined') {
+      throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before firebase-init.html');
+    }
+    firebase.firestore().enablePersistence()
+      .catch(function(err) {
+        if (err.code === 'failed-precondition') {
+          throw new Error('Multiple tabs open, persistence can only be enabled in one tab at a a time.');
+        } else if (err.code === 'unimplemented') {
+          throw new Error('The current browser does not support all of the features required to enable persistence');
+        }
+      });
+  }, 1000);
 })();
